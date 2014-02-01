73c64f91204adf6b0b902f443a1410c627cfa383
russellmcc <russell.mcclellan@gmail.com>
2014-02-01 16:35:43 -0500

    update.

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-bower install
-browserify -t coffeeify -t debowerify -t uglifyify knob.coffee --standalone knob -o knob.min.js
--- server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-bower install
-beefy knob.coffee:knob.min.js $npm_package_config_port -- -t coffeeify -t debowerify --standalone knob
